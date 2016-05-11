560acb7b082cbdd868cca3ffb5620310eb4e2d93
Jent Zheng <jent.zheng@gmail.com>
2016-05-11 15:10:50 +0800

    create gulpfile.js and npm init

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+// var livereload = require('gulp-livereload');
+// var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    // .pipe(autoprefixer({
+    //   browsers: ['last 2 versions'],
+    //   cascade: false
+    // }))
+    .pipe(gulp.dest('./css'))
+    // .pipe( livereload() );
+});
+
+gulp.task('sass:watch', function () {
+  // livereload.listen();
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
